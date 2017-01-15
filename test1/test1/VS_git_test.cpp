cf802fda4d25456cd4ab828833bf2c237475ca8f
liulongfei <liulongfei004@yahoo.com>
2017-01-15 17:09:49 +0800

    change the value!

--- test1/test1/VS_git_test.cpp
+++ test1/test1/VS_git_test.cpp
@@ -4,5 +4,9 @@ using namespace std;
 
 void main()
 {
-	cout << "hello world!" << endl;
+	string s = "liu,long,fei!!!";
+
+	for (auto &c : s)      //必须使用引用形式才能更改值
+		toupper(c);
+	cout << s << endl;
 }
